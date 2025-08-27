5ccaaf2cb8af338ebe1782fcfc020451875aac45
Medyanik-Mykola <medanik.mekola@gmail.com>
2025-08-27 17:05:02 +0300

    qwerty

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-let log_output = document.getElementById("log-output")
+let log_output = document.getElementById("status-log");
 
 function waiting(){
     new Promise((resolve, reject)=>{
     setTimeout(()=>{
-        log_output.innerHTML = "Очікування дій."
+        log_output.innerHTML =
+          "Лог системи: очікування нових повідомлень.";
         resolve()
     }, 800)
     }).then(()=>{
     return new Promise((resolve) => {
         setTimeout(() => {
-          log_output.innerHTML = "Очікування дій..";
+          log_output.innerHTML =
+            "Лог системи: очікування нових повідомлень..";
           resolve();
         }, 800);
       });
     }).then(()=>{
    return new Promise((resolve) => {
         setTimeout(() => {
-          log_output.innerHTML = "Очікування дій...";
+          log_output.innerHTML =
+            "Лог системи: очікування нових повідомлень...";
           resolve();
         }, 800);
       });
